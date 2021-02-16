c8a24b3775856e0555ae4351ebc863fb8e864eb3
Jim Calder <j@powerspike.co.uk>
2021-02-16 17:07:59 +0000

    finished chapter 23

--- symfony/assets/js/RepLog/RepLogCreator.js
+++ symfony/assets/js/RepLog/RepLogCreator.js
@@ -5,6 +5,10 @@ export default class RepLogCreator extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            quantityInputError: ''
+        };
+
         this.quantityInput = React.createRef();
         this.itemSelect = React.createRef();
 
@@ -25,8 +29,10 @@ export default class RepLogCreator extends Component {
         const itemSelect = this.itemSelect.current;
 
         if (quantityInput.value <= 0) {
-            // TODO - print some validation error!
-            
+            this.setState({
+                quantityInputError: 'Please enter a value greater than 0'
+            })
+
             // don't submit, or clear the form
             return;
         }
@@ -38,11 +44,15 @@ export default class RepLogCreator extends Component {
 
         itemSelect.selectedIndex = 0;
         quantityInput.value = '';
+        this.setState({
+            quantityInputError: ''
+        });
     }
 
     render() {
+        const { quantityInputError } = this.state;
         return (
-            <form className="form-inline" onSubmit={this.handleFormSubmit}>
+            <form onSubmit={this.handleFormSubmit}>
                 <div className="form-group">
                     <label className="sr-only control-label required" htmlFor="rep_log_item">
                         What did you lift?
@@ -62,7 +72,7 @@ export default class RepLogCreator extends Component {
                     </select>
                 </div>
                 {' '}
-                <div className="form-group">
+                <div className={`form-group ${quantityInputError ? 'has-error' : ''}`}>
                     <label className="sr-only control-label required" htmlFor="rep_log_reps">
                         How many times?
                     </label>
@@ -74,6 +84,7 @@ export default class RepLogCreator extends Component {
                         required="required"
                         type="number"
                     />
+                    {quantityInputError && <span className="help-block">{quantityInputError}</span>}
                 </div>
                 {' '}
                 <button type="submit" className="btn btn-primary">I Lifted it!</button>
