60354924c76184e4f845c69130b3b9925a57edf8
Jim Calder <j@powerspike.co.uk>
2021-02-17 19:26:29 +0000

    finished chapter 27

---
+++ symfony/assets/js/api/rep_log_api.js
@@ -0,0 +1,12 @@
+/**
+ * Returns a promise where the data is the rep log collection
+ *
+ * @return {Promise<Response>}
+ */
+
+export function getRepLogs() {
+    return fetch('/reps')
+        .then(response => {
+            return response.json();
+        })
+}
