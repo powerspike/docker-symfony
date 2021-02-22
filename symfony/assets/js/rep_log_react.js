23f4d950ed497198ca4c1f6ad5f7df38eeff04a4
Jim Calder <j@powerspike.co.uk>
2021-02-22 20:56:57 +0000

    finished chapter 40

--- symfony/assets/js/rep_log_react.js
+++ symfony/assets/js/rep_log_react.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import RepLogApp from './RepLog/RepLogApp';
 
-const itemOptions = [
-    { id: 'cat', text: 'Cat' },
-    { id: 'coffee_cup', text: 'Coffee Cup' },
-    { id: 'fat_cat', text: 'Big Fat Cat' },
-    { id: 'laptop', text: 'My Laptop' },
-    { id: 'invalid_item', text: 'Dark Matter' }
-];
-const shouldShowHeart = true;
+// const itemOptions = [
+//     { id: 'cat', text: 'Cat' },
+//     { id: 'coffee_cup', text: 'Coffee Cup' },
+//     { id: 'fat_cat', text: 'Big Fat Cat' },
+//     { id: 'laptop', text: 'My Laptop' },
+//     { id: 'invalid_item', text: 'Dark Matter' }
+// ];
+// const shouldShowHeart = true;
 
 render(
     <>
         <RepLogApp
-            itemOptions={itemOptions}
-            withHeart={shouldShowHeart}
+            // itemOptions={window.REP_LOG_APP_PROPS.itemOptions}
+            // withHeart={shouldShowHeart}
+            {...window.REP_LOG_APP_PROPS}
         />
     </>,
     document.getElementById('lift-stuff-app')
